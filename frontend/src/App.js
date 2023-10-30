fe075c60b50454832791c6c40bc1a4a723e170d0
Lollygagger <ubercharge60@gmail.com>
2023-10-30 10:14:32 -0400

    trying

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
 import "./App.css";
-import leftArrow from '/leftArrow.png';
+import leftArrow from 'leftArrow.png';
 
 
 const App = () => {
